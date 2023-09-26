0e7597e4bde4d22d7b20be3b87e3906ce638b30e
HarenKei <seungmin1266@gmail.com>
2023-09-26 22:50:54 +0900

    Add MinIntro.tsx

---
+++ pages/main/MainIntro.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const MainIntro = () => {
+  return (
+    <MainIntroContainer>
+    </MainIntroContainer>
+  );
+};
+
+const MainIntroContainer = styled.div`
+  width: 100%;
+  background-color: red;
+  
+`;
+
+export default MainIntro;
