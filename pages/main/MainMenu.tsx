981b84d0ace4ccd28111dfd40260c0e7696fd58e
HarenKei <seungmin1266@gmail.com>
2023-09-27 09:06:45 +0900

    Add menuArray.ts : Separate data array

--- pages/main/MainMenu.tsx
+++ pages/main/MainMenu.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Menu from "./Menu";
-import { useEffect } from "react";
+import { menuArray } from "./menuArray";
 
 interface Contents {
   conTitle: string;
@@ -12,97 +12,6 @@ interface Data {
   contents: Array<Contents>;
 }
 
-const menuArray = [
-  {
-    title: "BeACE?",
-    contents: [
-      {
-        conTitle: "BeACE 안내",
-        conLink: "https://job.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=120",
-      },
-      {
-        conTitle: "BeACE QnA",
-        conLink: "https://naver.com",
-      },
-    ],
-  },
-  {
-    title: "로드맵",
-    contents: [
-      {
-        conTitle: "BeACE 전체 프로그램",
-        conLink: "https://job.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=120",
-      },
-      {
-        conTitle: "Basic 프로그램",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "Educational 프로그램",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "Active 프로그램",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "Creative 프로그램",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "Ethical 프로그램",
-        conLink: "https://naver.com",
-      },
-    ],
-  },
-  {
-    title: "랭킹",
-    contents: [
-      {
-        conTitle: "전체 랭킹",
-        conLink: "https://job.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=120",
-      },
-      {
-        conTitle: "학과별 랭킹",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "학년별 랭킹",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "우리학과 랭킹",
-        conLink: "https://naver.com",
-      },
-      {
-        conTitle: "랭킹 시스템 소개",
-        conLink: "https://naver.com",
-      },
-    ],
-  },
-  {
-    title: "명예의 전당",
-    contents: [
-      {
-        conTitle: "1학점 명예의 전당",
-        conLink: "https://job.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=120",
-      },
-      {
-        conTitle: "총장상 명예의 전당",
-        conLink: "https://naver.com",
-      },
-    ],
-  },
-  {
-    title: "기타",
-    contents: [
-      {
-        conTitle: "개인정보 처리방침",
-        conLink: "https://job.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=120",
-      },
-    ],
-  },
-];
 
 const MainMenu = () => {
 
